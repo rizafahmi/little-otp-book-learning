e46c9dafb5dcb67bda228a6aa6bb457e57d18be0
Riza Fahmi <rizafahmi@gmail.com>
2016-10-02 10:06:59 +0700

    Refactoring, using default value in function

--- ch02/my_list.ex
+++ ch02/my_list.ex
@@ -1,18 +1,13 @@
 defmodule MyList do
+  def flatten([]), do: []
   def flatten([h|t]) do
     flatten(h) ++ flatten(t)
   end
   def flatten(h), do: [h]
-  def flatten([]), do: []
 
-  def sum([], result), do: result
-  def sum([]), do: 0
+  def sum([], result \\ 0), do: result
   def sum([h|t], result) do
     sum(h, result) + sum(t, result)
   end
-  def sum([h|t]) do
-    sum(h, 0) + sum(t, 0)
-  end
   def sum(h, result), do: h + result
-  def sum(h), do: sum(h, 0)
 end
