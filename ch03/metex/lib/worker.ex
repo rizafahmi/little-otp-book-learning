08fd844e7d9d5448cf27f2dcf0d79a0d0a690151
Riza Fahmi <rizafahmi@gmail.com>
2016-10-02 17:13:57 +0700

    Finish Metex app in chapter 3

--- ch03/metex/lib/worker.ex
+++ ch03/metex/lib/worker.ex
@@ -3,7 +3,7 @@ defmodule Metex.Worker do
   def loop do
     receive do
       {sender_pid, location} ->
-        send(sender_pid, temperature_of(location))
+        send(sender_pid, {:ok, temperature_of(location)})
       _ ->
         IO.puts "I don't know how to process this message"
     end
